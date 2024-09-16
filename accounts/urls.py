bb197587646411f5743988e6fb612520e359e943
Fazliddinn7 <fazliddingadoyev034@gmail.com>
2024-09-16 20:58:56 +0500

    create a user profile

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,10 @@
 from django.contrib.auth.views import LoginView
 from django.urls import path
 
-from accounts.views import logout_view
+from accounts.views import logout_view, profile_view
 
 urlpatterns = [
     path('login/', LoginView.as_view(redirect_authenticated_user=True), name='login_page'),
     path('logout/', logout_view, name='logout_page'),
+    path('profile/', profile_view, name='profile_page'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -5,3 +5,8 @@ from django.shortcuts import render
 def logout_view(request):
     logout(request)
     return render(request, 'registration/logout.html')
+
+
+def profile_view(request):
+    user = request.user
+    return render(request, 'pages/user_profile.html', {'user':user})
