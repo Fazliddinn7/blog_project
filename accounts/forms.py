d169cafe2fc33a20d9adbf59cf6335eaaf1793f4
Fazliddinn7 <fazliddingadoyev034@gmail.com>
2024-09-18 11:39:49 +0500

    sign up CreateView

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.contrib.auth.forms import UserCreationForm, BaseUserCreationForm
 from django.contrib.auth.models import User
 
 
@@ -7,15 +8,21 @@ class LoginForm(forms.Form):
     password = forms.CharField(widget=forms.PasswordInput)
 
 
-class UserRegistrationForm(forms.ModelForm):
-    password = forms.CharField(max_length=250, label='Password', widget=forms.PasswordInput)
-    password2 = forms.CharField(max_length=250, label='Confirm password', widget=forms.PasswordInput)
+# class UserRegistrationForm(forms.ModelForm):
+#     password = forms.CharField(max_length=250, label='Password', widget=forms.PasswordInput)
+#     password2 = forms.CharField(max_length=250, label='Confirm password', widget=forms.PasswordInput)
+#
+#     class Meta:
+#         model = User
+#         fields = 'username', 'email', 'first_name'
+#
+#     def clean_password2(self):
+#         if self.cleaned_data['password'] != self.cleaned_data['password2']:
+#             raise forms.ValidationError('Ikkita parol bir-biriga teng bo\'lishi kk')
+#         return self.cleaned_data['password2']
 
+
+class UserRegistrationForm(BaseUserCreationForm):
     class Meta:
         model = User
         fields = 'username', 'email', 'first_name'
-
-    def clean_password2(self):
-        if self.cleaned_data['password'] != self.cleaned_data['password2']:
-            raise forms.ValidationError('Ikkita parol bir-biriga teng bo\'lishi kk')
-        return self.cleaned_data['password2']
--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,9 @@
 from django.contrib.auth import logout
+from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views import View
+from django.views.generic import CreateView
 
 from accounts.forms import UserRegistrationForm
 
@@ -15,18 +18,24 @@ def profile_view(request):
     return render(request, 'pages/user_profile.html', {'user': user})
 
 
-class SignUpView(View):
+# class SignUpView(View):
+#
+#     def get(self, request):
+#         user_form = UserRegistrationForm()
+#         return render(request, 'account/register.html', {'user_form': user_form})
+#
+#     def post(self, request):
+#         user_form = UserRegistrationForm(request.POST)
+#         if user_form.is_valid():
+#             new_user = user_form.save(commit=False)
+#             new_user.set_password(
+#                 user_form.cleaned_data['password']
+#             )
+#             new_user.save()
+#         return render(request, 'account/register_done.html', {'user_form': user_form})
 
-    def get(self, request):
-        user_form = UserRegistrationForm()
-        return render(request, 'account/register.html', {'user_form': user_form})
 
-    def post(self, request):
-        user_form = UserRegistrationForm(request.POST)
-        if user_form.is_valid():
-            new_user = user_form.save(commit=False)
-            new_user.set_password(
-                user_form.cleaned_data['password']
-            )
-            new_user.save()
-        return render(request, 'account/register_done.html', {'user_form': user_form})
+class SignUpView(CreateView):
+    form_class = UserRegistrationForm
+    success_url = reverse_lazy('login_page')
+    template_name = 'account/register.html'
